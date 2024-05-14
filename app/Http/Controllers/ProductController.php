18b36461961c2ea3f71f74f516b7aeaa2d366835
Mishkat Hossain <hossainmishkat0@gmail.com>
2024-05-14 10:18:22 +0600

    new work

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    // show all product pages
+    public function index()
+    {
+
+    }
+
+    // show create product form
+    public function create()
+    {
+
+    }
+
+    public function store()
+    {
+
+    }
+
+    public function edit()
+    {
+
+    }
+    public function update()
+    {
+
+    }
+
+    public function destroy()
+    {
+        
+    }
+
+}
